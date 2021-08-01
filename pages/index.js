b7fe46ece0e10962ed11983f507ffb3551388ad2
Craig Puxty <craig_puxty@talktalk.net>
2021-08-01 21:21:27 +0100

    Outputting recipe data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import { createClient } from "contentful";
+import RecipeCard from "../components/RecipeCard";
 
 export async function getStaticProps() {
   const client = createClient({
@@ -15,7 +16,10 @@ const Recipes = ({ recipes }) => {
   console.log(recipes);
   return (
     <div>
-      <h2>Recipe List</h2>
+      <h2>Recipes</h2>
+      {recipes.map((recipe) => (
+        <RecipeCard key={recipe.sys.id} recipe={recipe} />
+      ))}
     </div>
   );
 };
