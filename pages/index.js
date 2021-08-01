bff699f037950970fc8f4751b64b7d6133714be5
Craig Puxty <craig_puxty@talktalk.net>
2021-08-01 20:49:09 +0100

    Added Contentful client

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,18 @@
-const Recipes = () => {
+import { createClient } from "contentful";
+
+export async function getStaticProps() {
+  const client = createClient({
+    space: process.env.SPACE,
+    accessToken: process.env.ACCESS_TOKEN,
+  });
+
+  const res = await client.getEntries({ content_type: "recipe" });
+
+  return { props: { recipes: res.items } };
+}
+
+const Recipes = ({ recipes }) => {
+  console.log(recipes);
   return (
     <div>
       <h2>Recipe List</h2>
