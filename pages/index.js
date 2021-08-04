cb9ee751f4762f2c66f828f1b2f19db559ea0e4f
Craig Puxty <craig_puxty@talktalk.net>
2021-08-04 21:38:00 +0100

    Generate paths

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
 import { createClient } from "contentful";
 import RecipeCard from "../components/RecipeCard";
 
-export async function getStaticProps() {
+export const getStaticProps = async () => {
   const client = createClient({
     space: process.env.SPACE,
     accessToken: process.env.ACCESS_TOKEN,
@@ -10,7 +10,7 @@ export async function getStaticProps() {
   const res = await client.getEntries({ content_type: "recipe" });
 
   return { props: { recipes: res.items } };
-}
+};
 
 const Recipes = ({ recipes }) => {
   console.log(recipes);
--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,6 +1,30 @@
-import React from "react";
+import { createClient } from "contentful";
 
-const RecipeDetails = () => {
+const client = createClient({
+  space: process.env.SPACE,
+  accessToken: process.env.ACCESS_TOKEN,
+});
+
+export const getStaticPaths = async () => {
+  const res = await client.getEntries({ content_type: "recipe" });
+  const paths = res.items.map((item) => {
+    return {
+      params: { slug: item.fields.slug },
+    };
+  });
+  return { paths, fallback: false };
+};
+
+export const getStaticProps = async ({ params }) => {
+  const { items } = await client.getEntries({
+    content_type: "recipe",
+    "fields.slug": params.slug,
+  });
+  return { props: { recipe: items[0] } };
+};
+
+const RecipeDetails = ({ recipe }) => {
+  console.log(recipe);
   return (
     <div>
       <h2>Recipe Details</h2>
