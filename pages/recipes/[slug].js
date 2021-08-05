8a7388c89e54b0552ae3bc015ad6360df7c21e41
Craig Puxty <craig_puxty@talktalk.net>
2021-08-05 10:14:09 +0100

    Added rich text content for recipe details

--- pages/recipes/[slug].js
+++ pages/recipes/[slug].js
@@ -1,4 +1,6 @@
 import { createClient } from "contentful";
+import Image from "next/image";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 const client = createClient({
   space: process.env.SPACE,
@@ -24,10 +26,31 @@ export const getStaticProps = async ({ params }) => {
 };
 
 const RecipeDetails = ({ recipe }) => {
-  console.log(recipe);
+  const { featuredImage, title, cookingTime, ingredients, method } =
+    recipe.fields;
   return (
-    <div>
-      <h2>Recipe Details</h2>
+    <div className="recipe-details">
+      <div className="recipe-details__banner">
+        <img
+          src={`https:${featuredImage.fields.file.url}`}
+          width={featuredImage.fields.file.details.image.width}
+          height={featuredImage.fields.file.details.image.height}
+        ></img>
+        <h2>{title}</h2>
+      </div>
+      <div className="recipe-details__info">
+        <p>Takes about {cookingTime} mins to cook.</p>
+        <h3>Ingredients:</h3>
+        <div className="recipe-details__ingredients">
+          {ingredients.map((ing) => (
+            <span key={ing}>{ing}</span>
+          ))}
+        </div>
+      </div>
+      <div className="recipe-details__method">
+        <h3>Method:</h3>
+        {documentToReactComponents(method)}
+      </div>
     </div>
   );
 };
